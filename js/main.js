6428c78e40d95f83eef9be65554549b019079623
JP_Escobari <jpe911@gmail.com>
2024-08-02 17:38:42 -0400

    Adding Constants and State Variables

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-  /*----- constants -----*/
+/*----- constants -----*/
 
+const deck = [
+  "dA", "dQ", "dK", "dJ", "d10", "d09", "d08", "d07", "d06", "d05", "d04", "d03", "d02",
+  "hA", "hQ", "hK", "hJ", "h10", "h09", "h08", "h07", "h06", "h05", "h04", "h03", "h02",
+  "cA", "cQ", "cK", "cJ", "c10", "c09", "c08", "c07", "c06", "c05", "c04", "c03", "c02",
+  "sA", "sQ", "sK", "sJ", "s10", "s09", "s08", "s07", "s06", "s05", "s04", "s03", "s02"
+];
 
-  /*----- state variables -----*/
+const payouts = {
+  "Royal Flush": 250,
+  "Straight Flush": 50,
+  "Four of a Kind": 25,
+  "Full House": 9,
+  "Flush": 6,
+  "Straight": 4,
+  "Three of a Kind": 3,
+  "Two Pair": 2,
+  "Jacks or Better": 1
+};
+
+/*----- state variables -----*/
+let playerHand = [];
+let playerCredits;
+let betSize;
+let gamePhase;
 
 
   /*----- cached elements  -----*/
